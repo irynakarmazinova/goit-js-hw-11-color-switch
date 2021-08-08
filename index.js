7d52dfce0cd300666fcd0e878e32b5db62020e07
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-08-08 17:47:49 +0300

    added function random color

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
 ];
 
 let intervalId = null;
@@ -13,11 +13,17 @@ let isActive = false;
 const btnStartRef = document.querySelector("[data-action='start']");
 const btnStopRef = document.querySelector("[data-action='stop']");
 
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 function onStartBtn() {
+  // btnStartRef.disabled = true;
+
   // если изменение темы запущено, то просто return из функции
   // если изменение темы не запущено, то запускаем - ставим буль и setInterval
   if (isActive) {
-    console.log("изменение темы запущено, кнопка старт не активна");
+    console.log('изменение темы запущено, кнопка старт не активна');
     return;
   }
 
@@ -31,18 +37,19 @@ function onStartBtn() {
     document.body.style.backgroundColor =
       colors[randomIntegerFromInterval(0, colors.length - 1)];
 
-    console.log("изменяю цвет фона");
+    console.log('изменяю цвет фона');
     // }, 5000); //если 5 сек, то будет видно, что кнопка старт повторно не нажимается, когда изменение темы запущено
   }, 1000);
 }
-btnStartRef.addEventListener("click", onStartBtn);
+btnStartRef.addEventListener('click', onStartBtn);
 
 function onStopBtn() {
-  clearInterval(intervalId, console.log("останавливаю изменения цвета фона"));
+  // if (!btnStartRef.disabled) {
+  //   return;
+  // }
+  // btnStartRef.disabled = false;
+
+  clearInterval(intervalId, console.log('останавливаю изменения цвета фона'));
   isActive = false;
 }
-btnStopRef.addEventListener("click", onStopBtn);
-
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+btnStopRef.addEventListener('click', onStopBtn);
