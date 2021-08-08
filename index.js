a702c2e2138d80191ae1d2af5a0f8dce6e11e694
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-08-08 19:59:55 +0300

    added solution on classes

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ function onStartBtn() {
 
     document.body.style.backgroundColor =
       colors[randomIntegerFromInterval(0, colors.length - 1)];
+    // getRandomColor();
 
     console.log('изменяю цвет фона');
     // }, 5000); //если 5 сек, то будет видно, что кнопка старт повторно не нажимается, когда изменение темы запущено
@@ -53,3 +54,77 @@ function onStopBtn() {
   isActive = false;
 }
 btnStopRef.addEventListener('click', onStopBtn);
+
+// function getRandomColor() {
+//   return `rgb${randomIntegerFromInterval(0, 255)}, ${randomIntegerFromInterval(
+//     0,
+//     255,
+//   )}, ${randomIntegerFromInterval(0, 255)}`;
+// }
+
+// ----------------------------------------------------------------------------------
+// второй вариант решения на классах от Димы ментора
+
+// const colors = [
+//   '#FFFFFF',
+//   '#2196F3',
+//   '#4CAF50',
+//   '#FF9800',
+//   '#009688',
+//   '#795548',
+// ];
+
+// class ColorSwitch {
+//   constructor(selector, colors) {
+//     this.selector = selector;
+//     this.colors = colors;
+
+//     this.intervalId = null;
+//   }
+
+//   getRefs() {
+//     const container = document.querySelector(this.selector);
+
+//     return {
+//       btnStartRef: container.querySelector("[data-action='start']"),
+//       btnStopRef: container.querySelector("[data-action='stop']"),
+//       body: document.body,
+//     };
+//   }
+
+//   randomIntegerFromInterval(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+//   }
+
+//   onStartBtn() {
+//     this.getRefs().btnStartRef.disabled = true;
+
+//     this.intervalId = setInterval(() => {
+//       this.getRefs().body.style.backgroundColor =
+//         this.colors[this.randomIntegerFromInterval(0, this.colors.length - 1)];
+//     }, 1000);
+//   }
+
+//   onStopBtn() {
+//     if (!this.getRefs().btnStartRef.disabled) {
+//       return;
+//     }
+
+//     clearInterval(this.intervalId);
+//     this.getRefs().btnStartRef.disabled = false;
+//   }
+
+//   addListeners() {
+//     this.getRefs().btnStartRef.addEventListener(
+//       'click',
+//       this.onStartBtn.bind(this),
+//     );
+//     this.getRefs().btnStopRef.addEventListener(
+//       'click',
+//       this.onStopBtn.bind(this),
+//     );
+//   }
+// }
+
+// const switchColor = new ColorSwitch('.container', colors);
+// switchColor.addListeners();
